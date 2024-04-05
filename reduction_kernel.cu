a5871a67a8b74e8347f9c4718728282fea6d250f
DengZheng <1031793990@qq.com>
2024-04-05 18:49:39 +0800

    [optimization] make first reduction happens during load data in global memory into shared memory.

--- reduction_kernel.cu
+++ reduction_kernel.cu
@@ -3,6 +3,7 @@
 #include <vector>
 #include <random>
 #include <chrono>
+#include <cassert>
 
 #define ROW_NUM 16
 #define COL_NUM 256
@@ -19,7 +20,20 @@ std::vector<float> init_random_vector(int length) {
     return random_vector;
 }
 
-template<typename T>
+std::vector<float> cpu_verification(const std::vector<float>& in, int row, int col) {
+    assert(row * col == in.size());
+    std::vector<float> ret;
+    for (int i = 0; i < row; i++) {
+        float temp = 0.f;
+        for (int j = 0; j < col; j ++) {
+            temp += in[i * col + j];
+        }
+        ret.push_back(temp);
+    }
+    return ret;
+}
+
+template <typename T>
 __global__ void reduction0(T* __restrict__ in, T* __restrict__ out) {
     extern __shared__ T sdata[];
     unsigned int tid = threadIdx.x;
@@ -39,6 +53,28 @@ __global__ void reduction0(T* __restrict__ in, T* __restrict__ out) {
     }
 }
 
+// split one line one block here into one line two block
+template <typename T>
+__global__ void reduction1(T* __restrict__ in, T* __shared__ out) {
+    extern __shared__ T sdata[];
+    unsigned int tid = threadIdx.x;
+    unsigned int i   = blockIdx.x * blockDim.x * 2 + tid;
+    sdata[tid] = in[i] + in[i + blockDim.x];
+    __syncthreads();
+
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (tid < s) {
+            sdata[tid] += sdata[tid + s];
+        }
+        __syncthreads();
+    }
+    if (tid == 0) {
+        out[blockIdx.x] = sdata[0];
+    }
+}
+
+
+
 int main() {
     std::vector<float> h_in = init_random_vector(ROW_NUM * COL_NUM);
     std::vector<float> h_out(ROW_NUM, 0.f);
@@ -48,12 +84,16 @@ int main() {
     cudaMalloc(&d_out, sizeof(float) * ROW_NUM);
 
     cudaMemcpy(d_in, h_in.data(), sizeof(float) * h_in.size(), cudaMemcpyHostToDevice);
-    reduction0<<<ROW_NUM, COL_NUM, COL_NUM * sizeof(float)>>>(d_in, d_out);
+    // reduction0<<<ROW_NUM, COL_NUM, COL_NUM * sizeof(float)>>>(d_in, d_out);
+    reduction1<<<ROW_NUM, COL_NUM / 2, COL_NUM / 2 * sizeof(float)>>>(d_in, d_out);
+    auto cpu_result = cpu_verification(h_in, ROW_NUM, COL_NUM);
+
+
     cudaDeviceSynchronize();
     cudaMemcpy(h_out.data(), d_out, sizeof(float) * h_out.size(), cudaMemcpyDeviceToHost);
 
     for (int i = 0; i < h_out.size(); i++) {
-        std::cout << h_out[i] << std::endl;
+        std::cout << "cuda : " <<h_out[i] << ", cpu : " << cpu_result[i] << std::endl;
     }
 
     cudaFree(d_in);
