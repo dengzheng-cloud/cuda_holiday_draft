34e7be9e50440f47cf32b028c233b4fba3aace8f
DengZheng <1031793990@qq.com>
2024-04-06 04:14:16 +0800

    [optimization] unroll loop and reduction multiple element per thread, while i have no idea if i complete that.

--- reduction_kernel.cu
+++ reduction_kernel.cu
@@ -6,7 +6,18 @@
 #include <cassert>
 
 #define ROW_NUM 16
-#define COL_NUM 256
+#define COL_NUM 512
+
+template <typename T>
+__device__ void warp_reduction(volatile T* sdata, int tid) {
+    sdata[tid] += sdata[tid + 32];
+    sdata[tid] += sdata[tid + 16];
+    sdata[tid] += sdata[tid +  8];
+    sdata[tid] += sdata[tid +  4];
+    sdata[tid] += sdata[tid +  2];
+    sdata[tid] += sdata[tid +  1];
+}
+
 
 std::vector<float> init_random_vector(int length) {
     std::random_device rd;
@@ -74,10 +85,74 @@ __global__ void reduction1(T* __restrict__ in, T* __shared__ out) {
 }
 
 
+// split one line one block here into one line two block
+template <typename T>
+__global__ void reduction2(T* __restrict__ in, T* __restrict__ out) {
+    extern __shared__ T sdata[];
+    unsigned int tid = threadIdx.x;
+    unsigned int i   = blockIdx.x * blockDim.x * 2 + tid;
+    sdata[tid] = in[i] + in[i + blockDim.x];
+    __syncthreads();
+
+    for (unsigned int s = blockDim.x / 2; s > 32; s >>= 1) {
+        if (tid < s) {
+            sdata[tid] += sdata[tid + s];
+        }
+        __syncthreads();
+    }
+    if (tid < 32) { warp_reduction<T>(sdata, tid); }
+
+    if (tid == 0) {
+        out[blockIdx.x] = sdata[0];
+    }
+}
+
+
+template <typename T, int block_size>
+__device__ void warp_reduction1(volatile T* sdata, int tid) {
+    if (block_size >= 64) sdata[tid] += sdata[tid + 32];
+    if (block_size >= 32) sdata[tid] += sdata[tid + 16];
+    if (block_size >= 16) sdata[tid] += sdata[tid +  8];
+    if (block_size >=  8) sdata[tid] += sdata[tid +  4];
+    if (block_size >=  4) sdata[tid] += sdata[tid +  2];
+    if (block_size >=  2) sdata[tid] += sdata[tid +  1];
+}
+
+
+
+template <typename T, int block_size>
+__global__ void reduction3(T* __restrict__ in, T* __restrict__ out, unsigned int n) {
+    extern __shared__ T sdata[];
+    unsigned int tid = threadIdx.x;
+    unsigned int offset = blockIdx.x * COL_NUM;
+    unsigned int i   = tid;
+    
+    while (i < n) {
+        sdata[tid] += in[offset + i] + in[offset + i + block_size]; 
+        i += block_size * 2;
+    }
+    __syncthreads();
+
+    for (unsigned int s = blockDim.x / 2; s > 32; s >>= 1) {
+        if (tid < s) {
+            sdata[tid] += sdata[tid + s];
+        }
+        __syncthreads();
+    }
+    if (tid < 32) { warp_reduction1<T, block_size>(sdata, tid); }
+
+    if (tid == 0) {
+        out[blockIdx.x] = sdata[0];
+    }
+}
+
+
 
 int main() {
     std::vector<float> h_in = init_random_vector(ROW_NUM * COL_NUM);
     std::vector<float> h_out(ROW_NUM, 0.f);
+
+    const int block_size = 64;
 
     float *d_in, *d_out;
     cudaMalloc(&d_in, sizeof(float) * ROW_NUM * COL_NUM);
@@ -85,7 +160,9 @@ int main() {
 
     cudaMemcpy(d_in, h_in.data(), sizeof(float) * h_in.size(), cudaMemcpyHostToDevice);
     // reduction0<<<ROW_NUM, COL_NUM, COL_NUM * sizeof(float)>>>(d_in, d_out);
-    reduction1<<<ROW_NUM, COL_NUM / 2, COL_NUM / 2 * sizeof(float)>>>(d_in, d_out);
+    // reduction1<<<ROW_NUM, COL_NUM / 2, COL_NUM / 2 * sizeof(float)>>>(d_in, d_out);
+    // reduction2<<<ROW_NUM, COL_NUM / 2, COL_NUM / 2 * sizeof(float)>>>(d_in, d_out);
+    reduction3<float, block_size><<<ROW_NUM, block_size, block_size * sizeof(float)>>>(d_in, d_out, COL_NUM);
     auto cpu_result = cpu_verification(h_in, ROW_NUM, COL_NUM);
 
 
